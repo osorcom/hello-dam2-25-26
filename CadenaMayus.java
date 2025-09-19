571eacd0c7bbb9cfd17c818c40550d674fc5d6f5
Joan_Rock <joanlopezclotet@gmail.com>
2025-09-19 16:09:21 +0200

    mmayus

---
+++ CadenaMayus.java
@@ -0,0 +1,7 @@
+class CadenaMayus{
+    public static String mayus(String cad){
+        return cad.toUpperCase();
+    }
+
+
+}
--- Hello.java
+++ Hello.java
@@ -2,9 +2,15 @@ class Hello{
   public static void main(String[] args){
     System.out.println("Hola DAM 2");
 
+
+
+
+    System.out.println("Joan");
+
+
+
     System.out.println("Juan");
     System.out.println("Otro sout");
-    System.out.println("Joan");
 
   }
 }
